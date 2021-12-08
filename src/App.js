82e51dcc6812b323186a279861d83b58bb0dc559
Debnath <z0094433@vcs.privad.net>
2021-12-08 12:58:24 +0530

    tweaks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Weather from "./components/Weather";
 
 function App() {
+  useEffect(() => {
+    document.title = "Weather app"
+ }, []);
+
   return (
     <div className="App">
       <Weather />
