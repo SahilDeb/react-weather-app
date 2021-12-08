d532bdbde760ca604a02fbd528b8d21b70af74fe
Debnath <z0094433@vcs.privad.net>
2021-12-08 12:48:05 +0530

    initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+function Weather() {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+  });
+
+  const APIKEY = "dc9f9be4984cb50a8613b8095891598d";
+  async function weatherData(e) {
+    e.preventDefault();
+    if (form.city === "") {
+      alert("Add values");
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city}&APPID=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+      setWeather({ data: data });
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "city") {
+      setForm({ ...form, city: value });
+    }
+  };
+  return (
+    <div className="weather">
+      <span className="title">Check Weather</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="enter city name"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="getweather" onClick={(e) => weatherData(e)}>
+          Submit
+        </button>
+      </form>
+
+      {weather.data !== undefined ? (
+        <div>
+          <DisplayWeather data={weather.data} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Weather;
